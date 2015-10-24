fef5696afd47c9fe923caea3f98db6203ae9b105
Branko Vukelic <branko@outernet.is>
2015-10-24 18:57:17 +0200

    Cache device data during single instantiation

--- hwd/wrapper.py
+++ hwd/wrapper.py
@@ -7,18 +7,21 @@ class Wrapper:
 
     def __init__(self, dev):
         self.name = dev.sys_name
+        self._device = None
 
     @property
     def device(self):
         # We always create a new context and look up the devices because they
         # may disappear or change their state between lookups.
-        ctx = pyudev.Context()
-        devs = ctx.list_devices(sys_name=self.name)
-        try:
-            return list(devs)[0]
-        except IndexError:
-            raise ValueError('Device {} no longer present in context'.format(
-                self.name))
+        if not self._device:
+            ctx = pyudev.Context()
+            devs = ctx.list_devices(sys_name=self.name)
+            try:
+                self._device = list(devs)[0]
+            except IndexError:
+                raise ValueError(
+                    'Device {} no longer present in context'.format(self.name))
+        return self._device
 
     @property
     def system_path(self):
