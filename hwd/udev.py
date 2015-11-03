3a829634cc4658dc9a5f29e176653f2baa9bae87
Branko Vukelic <branko@outernet.is>
2015-11-03 15:50:05 +0100

    Fxied usage exmaple
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- hwd/udev.py
+++ hwd/udev.py
@@ -12,7 +12,7 @@ def devices_by_subsystem(subsys, only=lambda x: True):
 
     Example::
 
-        >>> get_devices_by_subsystem('net')
+        >>> devices_by_subsystem('net')
         [Device('/sys/devices/pci0000:00/0000:00:1c.3/0000:02:00.0/net/wlp2s0'),
          Device('/sys/devices/virtual/net/lo')]
     """
